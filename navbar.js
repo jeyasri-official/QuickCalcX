e74db20f6eb59402aef3d75f6ddaa3da6dfb3209
jeyasri-official <jeyasrisantharam@gmail.com>
2025-03-20 19:04:37 +0530

    Initial Commit

---
+++ navbar.js
@@ -0,0 +1,22 @@
+function openMenu() {
+    document.getElementById("fullscreenMenu").classList.add("show");
+    document.querySelector(".nav-links").style.display = "none"; // Hide desktop menu
+}
+
+function closeMenu() {
+    document.getElementById("fullscreenMenu").classList.remove("show");
+    document.querySelector(".nav-links").style.display = "flex"; // Show desktop menu
+}
+
+// Function to toggle mobile dropdowns
+function toggleDropdown(id) {
+    let dropdown = document.getElementById(id);
+    dropdown.classList.toggle("show");
+}
+
+fetch("../navbar.html")  
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("navbar-container").innerHTML = data;
+    })
+    .catch(error => console.error("Error loading navbar:", error));
