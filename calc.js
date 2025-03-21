4e9e166144e5a3d410df655cba2c598e88b9ebde
jeyasri-official <jeyasrisantharam@gmail.com>
2025-03-21 12:39:23 +0530

    Updated Version

--- calc.js
+++ calc.js
@@ -1,43 +1,59 @@
 let input = document.querySelector("input");
-
-// Function to handle button clicks
+let resultDisplayed = false; // Track if the result is displayed
 function addValue(userInput) {
-    if (userInput === "%") {
-        input.value += "%"; // Just add '%' without changing the number
-    } else {
+    let lastChar = input.value.slice(-1);
+
+    // If a result is displayed and the user enters a number, reset input
+    if (resultDisplayed && !isNaN(userInput)) {
+        input.value = userInput;
+        resultDisplayed = false;
+    } 
+    // If a result is displayed and the user enters an operator, continue calculation
+    else if (resultDisplayed && isOperator(userInput)) {
         input.value += userInput;
+        resultDisplayed = false;
+    } 
+    else {
+        // If the last character is %, and user enters a number, add '*'
+        if (lastChar === "%" && !isNaN(userInput)) {
+            input.value += "*" + userInput;
+        } else {
+            input.value += userInput;
+        }
     }
 }
 
-// Function to clear the input field
-function clearVal() {
-    input.value = "";
+// Check if the input is an operator
+function isOperator(char) {
+    return ["+", "-", "*", "/", "%"].includes(char);
 }
 
-// Function to delete the last character
+function clearVal() {
+    input.value = "";
+    resultDisplayed = false;
+}
 function delValue() {
     input.value = input.value.slice(0, -1);
 }
-
-// Function to evaluate the expression
 function total() {
     try {
-        // Convert percentages: replace "50%" with "0.5"
         let expression = input.value.replace(/(\d+(\.\d+)?)%/g, (match, num) => {
-            return parseFloat(num) / 100;
+            return "(" + (parseFloat(num) / 100) + ")";
         });
 
-        input.value = eval(expression); // Evaluate the expression
+        input.value = eval(expression); 
+        resultDisplayed = true; 
 
     } catch {
-        input.style.color = "red"; 
+        input.style.color = "red";
         input.style.fontSize = "25px";
         input.value = "Oops! Enter Correct Syntax!";
-
+        
         setTimeout(() => {
-            input.style.color = ""; 
-            input.style.fontSize = ""; 
-            input.value = ""; 
+            input.style.color = "";
+            input.style.fontSize = "";
+            input.value = "";
         }, 1500);
+        resultDisplayed = false;
     }
 }
